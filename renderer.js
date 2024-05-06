01990e045d642d7e3796124576b944a5bab348ad
Kacper Akdag-Ochnik <kacperochnik01@gmail.com>
2024-05-06 09:25:39 +0200

    added dark mode toggle

---
+++ renderer.js
@@ -0,0 +1,4 @@
+document.getElementById('hide-checkbox').addEventListener('click', async () => {
+    const isDarkMode = await window.darkMode.toggle()
+    document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
+  })
